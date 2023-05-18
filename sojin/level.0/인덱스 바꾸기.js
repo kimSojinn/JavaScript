367a6d4bf200887c0c84799fa76500cd8c931589
Sojin <118108221+kimSojinn@users.noreply.github.com>
2023-05-18 16:25:12 +0900

    2023.05.18 (#49)
    피드백 반영 for문 사용한 풀이 추가했습니다.

--- sojin/level.0/인덱스 바꾸기.js
+++ sojin/level.0/인덱스 바꾸기.js
@@ -14,6 +14,23 @@ function solution(my_string, num1, num2) {
   return array.join("");
 }
 
+/**
+ * for문으로 작성한 풀이
+ *function solution(my_string, num1, num2) {
+  let answer = "";
+  for (let i = 0; i < my_string.length; i++) {
+    if (i === num1) {
+      answer += my_string[num2];
+    } else if (i === num2) {
+      answer += my_string[num1];
+    } else {
+      answer += my_string[i];
+    }
+  }
+  return answer;
+}
+ */
+
 function main() {
   const my_string = "hello";
   const num1 = 1;
