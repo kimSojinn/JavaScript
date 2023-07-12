a23dfc2ba70587faac4dad980a3a7c9dff42de55
Sojin <118108221+kimSojinn@users.noreply.github.com>
2023-07-12 23:10:03 +0900

    2023.07.12 (#71)
    - 피드백 받은 부분 수정 + 문제 풀이

--- sojin/level.1/짝수와 홀수.js
+++ sojin/level.1/짝수와 홀수.js
@@ -9,11 +9,7 @@ import { checkAnswer } from "../../common.js";
 정수 num이 짝수일 경우 "Even"을 반환하고 홀수인 경우 "Odd"를 반환하는 함수, solution을 완성해주세요. **/
 
 function solution(num) {
-  if (num % 2 === 0) {
-    return "Even";
-  } else {
-    return "Odd";
-  }
+  return num % 2 === 0 ? "even" : "Odd";
 }
 
 function main() {
--- sojin/level.1/평균 구하기.js
+++ sojin/level.1/평균 구하기.js
@@ -11,17 +11,18 @@ import { checkAnswer } from "../../common.js";
 function solution(arr) {
   let answer = 0;
 
-  for (let i = 0; i < arr.length; i++) {
-    answer += arr[i];
+  for (const num of arr) {
+    answer += num;
   }
   return answer / arr.length;
 }
 
-// reduce 메서드를 사용한 풀이
+/*
+reduce 메서드를 사용한 풀이
 function solution(arr) {
   const answer = arr.reduce((acc, num) => acc + num, 0);
   return answer / arr.length;
-}
+}*/
 
 function main() {
   const arr = [1, 2, 3, 4];
