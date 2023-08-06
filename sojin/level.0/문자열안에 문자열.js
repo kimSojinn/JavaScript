3bac8278c1b696c622d2736314a8b2e888d0fd62
Sojin <118108221+kimSojinn@users.noreply.github.com>
2023-08-06 20:10:59 +0900

    0단계 다양한 방법으로 풀이하기 (#78)

--- sojin/level.0/문자열안에 문자열.js
+++ sojin/level.0/문자열안에 문자열.js
@@ -12,6 +12,18 @@ function solution(str1, str2) {
   return str1.includes(str2) ? 1 : 2;
 }
 
+/** split을 사용한 풀이
+ * function solution(str1, str2) {
+    return str1.split(str2).length > 1 ? 1 : 2
+}
+ */
+
+/**  indexOf는 특정 문자열이 처음으로 나타나는 인덱스를 반환, 재하지 않는다면-1을 반환하는 특징을 이용한 풀이
+ * function solution(str1, str2) {
+    return str1.indexOf(str2) === -1 ? 2 : 1;
+}
+ */
+
 function main() {
   const str1 = "ab6CDE443fgh22iJKlmn1o";
   const str2 = "6CD";
